d9cb0b6b4ac816195edcd1c9e75aa6e0d7c0f50d
Daia Bromberg <bromd514@newschool.edu>
2025-02-03 13:56:26 -0500

    next batch of pages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // Define total number of pages
 const totalPages = 20;
-const variations = ["WEIGHT", "Variation B", "Variation C", "Variation D", "Variation E"];
-
+const variations = ["WEIGHT", "FONT", "SLANT", "BORDER", "SPACE"];
 
 // Get elements
 const pageInfo = document.getElementById("page-info");
@@ -17,7 +16,7 @@ currentPage = Math.max(1, Math.min(currentPage, totalPages));
 
 // Define the base text for all pages
 const baseText = `
-    <span class="line-1">It’s turned <span class="word-cold">cold</span>: <span class="word-cold">cold</span> so that saucers of 
+    <span class="line-1">It’s turned <span class="word-cold">cold</span>: <span class="word-cold">cold</span> so that <span class="word-saucers">saucers</span> of 
     <span class="word-ice">ice</span> lie in the mud, blank and crazed as antique porcelain.</span> 
     
     <span class="line-2"><span class="word-cold">Cold</span> so the hedges are alive with Baltic 
@@ -32,11 +31,11 @@ const baseText = `
     <span class="line-5">My feet crack the <span class="word-ice">ice</span> in the mud as I trudge uphill.</span> 
     
     <span class="line-6">And because the squeaks and grinding <span class="word-harmonics">harmonics</span> 
-    of fracturing ice sound to Mabel like a wounded animal, every step I take is met with a convulsive 
+    of <span class="word-fracturing">fracturing </span>ice sound to Mabel like a <span class="word-wounded">wounded</span> animal, every step I take is met with a <span class="word-convulsive">convulsive</span> 
     <span class="word-clench">clench</span> of her toes.</span> 
     
     <span class="line-7">Where the world isn’t <span class="word-white">white</span> with frost, it’s striped green and 
-    brown in strong sunlight, so the land is particoloured and <span class="word-snapping">snapping</span> 
+    brown in strong sunlight, so the land is <span class="word-particoloured">particoloured</span> and <span class="word-snapping">snapping</span> 
     backwards to dawn and forwards to dusk.</span> 
     
     <span class="line-8">The days, now, are a <span class="word-bare">bare</span> six hours long.</span>
